28e239f39fea74eadbc75d64a4878415dd572c9b
David Lee <david168@uw.edu>
2017-01-06 10:26:49 -0800

    examples or variable declarations in R

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,23 +2,39 @@
 
 # Create a variable `hometown` that stores the city in which you were born
 
+hometown.city <- Bellevue
+print(hometown.city)
 
 # Assign your name to the variable `my.name`
 
+my.name <- David
+print(my.name)
 
 # Assign your height (in inches) to a variable `my.height`
 
+height.inches <- 69
+print(height.inches)
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
 
+puppies <- 1
+print(puppies)
 
 # Create a variable `puppy.price`, which is how expensive you think a puppy is
 
+puppy.price <- 200
+print(puppy.price)
 
 # Create a variable `total.cost` that has the total cost of all of your puppies
 
+total.cost <- puppies * puppy.price
 
 # Create a boolean variable `too.expensive`, set to TRUE if the cost is greater than $1,000
 
+too.expensive <- 1000
 
 # Create a variable `max.puppies`, which is the number of puppies you can afford for $1,000.
+
+max.puppies <- too.expensive / puppy.price
+print(max.puppies)
+
